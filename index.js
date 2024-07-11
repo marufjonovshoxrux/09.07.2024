7bd4601c6028c65cd8d0ce4ad6400a5458eecf13
marufjonovshoxrux <shokha0502@gmail.com>
2024-07-11 21:33:52 +0500

    main

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import { storys } from "./components/story.js"
 import { reload } from "./js/utils.js"
+import { post } from "./components/post.js"
 
 const story = document.querySelector('.story')
+const posts = document.querySelector('.posts')
 
 fetch('https://jsonplaceholder.typicode.com/photos?_start=100&_limit=200')
     .then(res => res.json())
     .then(res => reload(res,story,storys))
+
+fetch('https://jsonplaceholder.typicode.com/photos?_start=0&_limit=100')
+    .then(res => res.json())
+    .then(res => reload(res, posts, post))
